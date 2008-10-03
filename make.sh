a2ecf40da88b5bac0c4fb805bc30b6799dd374cc
Michael Tremer <michael.tremer@ipfire.org>
2008-10-03 22:47:15 +0000

    Bump toolchain version.

--- make.sh
+++ make.sh
@@ -25,7 +25,7 @@
 NAME="IPFire"			# Software name
 SNAME="ipfire"			# Short name
 VERSION="3.0-prealpha"		# Version number
-TOOLCHAINVERSION="${VERSION}-6"	# Toolchain
+TOOLCHAINVERSION="${VERSION}-7"	# Toolchain
 SLOGAN="Lechery"		# Software slogan
 
 # Include funtions
