dbb78edd5996b29b57adc8d18bfc032926b560c7
Michael Tremer <michael.tremer@ipfire.org>
2009-02-06 11:28:16 +0100

    Changed name to IPFire-3.0-alpha1 (Lechery).

--- make.sh
+++ make.sh
@@ -24,8 +24,8 @@
 
 NAME="IPFire"			# Software name
 SNAME="ipfire"			# Short name
-VERSION="3.0-prealpha1"		# Version number
-TOOLCHAINVERSION="${VERSION}-18"	# Toolchain
+VERSION="3.0-alpha1"		# Version number
+TOOLCHAINVERSION="${VERSION}-1"	# Toolchain
 SLOGAN="Lechery"		# Software slogan
 
 # Include funtions
