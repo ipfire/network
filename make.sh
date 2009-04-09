cb9bb118d0ba98fa5ba592d1d5184bea161a3081
Michael Tremer <michael.tremer@ipfire.org>
2009-04-09 10:55:20 +0200

    Renamed squashfs to squashfs-tools. Update to 4.0.

--- make.sh
+++ make.sh
@@ -291,7 +291,7 @@ ipfire_build() {
 	ipfire_make reiser4progs
 	ipfire_make xfsprogs
 	ipfire_make sysfsutils
-	ipfire_make squashfs
+	ipfire_make squashfs-tools
 	ipfire_make dosfstools
 	ipfire_make lvm2
 	ipfire_make mdadm
