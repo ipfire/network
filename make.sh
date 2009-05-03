dc2b2305f7e3a95378019b054cd548bb46703807
Michael Tremer <michael.tremer@ipfire.org>
2009-05-03 10:52:16 +0200

    Removed libusb-compat.

--- make.sh
+++ make.sh
@@ -209,7 +209,6 @@ ipfire_build() {
 	ipfire_make libxslt
 	ipfire_make popt
 	ipfire_make libusb
-	ipfire_make libusb-compat	# Can be removed if usbutils supports libusb-1.0
 	ipfire_make bc
 	ipfire_make mingetty
 	ipfire_make boost
