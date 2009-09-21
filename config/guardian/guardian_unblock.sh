b5bd7c09783f511a49e88235f27b3e79f84fd31c
Michael Tremer <michael.tremer@ipfire.org>
2009-09-21 21:30:09 +0200

    Remove guardian.
    This got already replaced.

--- config/guardian/guardian_unblock.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# this is a sample unblock script for guardian. This should work with ipchains.
-# This command gets called by guardian as such:
-#  unblock.sh <source_ip>
-# and the script will issue a command to remove the block that was created with # block.sh address.
-source=$1
-
-/sbin/iptables -D INPUT -s $source -j DROP
